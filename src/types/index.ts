465194fde91dfc70a46d5a168eed027d99343390
Kallah94 <fallmoussa994@gmail.com>
2025-07-18 13:21:56 +0000

    add spaceobjectconfig interface

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,3 +1,10 @@
 import * as THREE from 'three';
 
 // Types de base pour les objects spatiaux
+export interface SpaceObjectConfig {
+    name: string;
+    mass: number; // kg
+    position: THREE.Vector3;
+    velocity: THREE.Vector3;
+}
+
