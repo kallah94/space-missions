677a7e98d7b1e5f238a85f8a84f4933790c60abd
Kallah94 <fallmoussa994@gmail.com>
2025-07-18 13:38:00 +0000

    add OrbitalElementsTypes

--- src/types/index.ts
+++ src/types/index.ts
@@ -23,3 +23,35 @@ export interface EngineConfig {
     efficiency: number; // Efficacité du moteur (0-1)
 }
 
+// Types pour les planètes
+export interface PlanetConfig extends SpaceObjectConfig {
+    radius: number; // km
+    rotationPeriod: number; // s
+    atmosphere?: AtmosphereConfig
+    j2?: number; // Coefficient d'aplatissement
+    gravityModel?: GravityModelConfig;
+}
+
+export interface AtmosphereConfig {
+    composition: string[]; // Ex: ["CO2", "N2", "O2"]
+    pressure: number; // Pa
+    temperature: number; // K
+}
+
+export interface GravityModelConfig {
+    mu: number; // Constante gravitationnelle (m^3/s^2)
+    j2?: number; // Coefficient J2
+    j3?: number; // Coefficient J3
+    j4?: number; // Coefficient J4
+}
+
+// Types pour les éléments orbitaux
+export interface OrbitalElements {
+    semiMajorAxis: number; // km
+    eccentricity: number; // 0-1
+    inclination: number; // radians
+    longitudeOfAscendingNode: number; // radians
+    argumentOfPeriapsis: number; // radians
+    trueAnomaly: number; // radians
+    epoch: Date; // Date de référence pour les éléments orbitauxg
+}
